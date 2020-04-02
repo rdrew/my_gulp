fe0ad4a3bdab50bb645e2456f4378b00dc45aec9
rdrew <rdrew@upei.ca>
2020-04-02 15:28:50 -0300

    alot

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,35 @@
-//@format
-var sourcemaps = require('gulp-sourcemaps');
+'use strict';
+
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var $ = require('gulp-load-plugins')();
-var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence  = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 var yaml = require('js-yaml');
 var fs = require('fs');
-var imagemin = require('gulp-imagemin');
+//var cache = require('gulp-cache');
 
-var {
-  COMPATIBILITY,
-  PORT,
-  PROXY,
-  LOCALPROXY,
-  THEMENAME,
-  UNCSS_OPTIONS,
-  PATHS,
-} = loadConfig();
+var {SITE, PORT, BSREWRITE, PATHS} = loadConfig();
 
 function loadConfig() {
   var ymlFile = fs.readFileSync('config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
-//
 
-function sass() {
-  return gulp
-    .src(PATHS.Scss.Dir + '/' + PATHS.Scss.FileName, {sourcemaps: true})
-    .pipe(sourcemaps.init())
-    .pipe(
-      $.sass({
-        includePaths: PATHS.Scss.Libraries,
-        outputStyle: 'compressed', // if css compressed **file size**
-      }).on('error', $.sass.logError),
-    )
-    .pipe($.postcss([autoprefixer()]))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(PATHS.Css.Dir))
-    .pipe(browserSync.stream());
-}
+//===================
+// Browsersync Proxy
+//===================
 
-// Copy images to the "dist" folder
-// In production, the images are compressed
-function images() {
-  return gulp
-    .src(PATHS.Img.Src + '/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest(PATHS.Img.Dest));
-}
-
-function remoteProxy() {
-  browserSync.init({
+gulp.task('server', function() {
+  server.init({
+    proxy: SITE.Remote.Url,
     serveStatic: ['.'],
-    proxy: SITE.Remote,
-    startPath: SITE.Remote.StartPath,
-    injectChanges: true,
-    files: ['css/*.css', 'js/*.js'],
+    //files: ['./css/*.css', './js/*.js'],
+    files: PATHS.Watch,
     plugins: ['bs-rewrite-rules'],
     rewriteRules: [
       {
@@ -68,15 +42,88 @@ function remoteProxy() {
       },
     ],
   });
-}
+});
 
-function watch() {
-  gulp.watch(PATHS.ScssDir + '/*.scss', sass);
-}
+//====================
+// Sass Compilation
+//===================
 
-gulp.task('sass', sass);
-gulp.task('images', images);
-gulp.task('remoteProxy', remoteProxy);
-gulp.task('watch', watch);
+gulp.task('sass', function() {
+  return gulp
+    //.src('./src/scss/**/*.scss')
+    .src(PATHS.Scss.Dir + '/*.scss')
+    //.src(PATHS.Scss.Dir + '/' + PATHS.Scss.FileName, {sourcemaps: true})
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        includePaths: PATHS.Scss.Libraries,
+      }).on('error', sass.logError),
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(PATHS.Css.Dir))
+    .pipe(server.stream());
+});
 
-gulp.task('default', gulp.series('sass', 'images', 'remoteProxy', 'watch'));
+
+
+//====================
+// JS Concatination
+//===================
+
+gulp.task('js', function() {
+  gulp
+    //.src(PATHS.Js.Src) // path to your files
+    .src(PATHS.Js.Src) // path to your files
+    .pipe(concat(PATHS.Js.FileName)) // concat and name it "concat.js"
+    .pipe(gulp.dest(PATHS.Js.Dest));
+});
+
+//====================
+// Image Optimization
+//===================
+
+gulp.task('images', function() {
+  gulp
+    .src(PATHS.Img.Src + '/**/*')
+    //.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(
+      imagemin([
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+          plugins: [{removeViewBox: true}, {cleanupIDs: false}],
+        }),
+      ]),
+    )
+    .pipe(gulp.dest(PATHS.Img.Dest));
+});
+
+
+// ##################
+// Watch Task
+// ##################
+
+gulp.task('watch', ['server', 'sass'], function() {
+  //watch sass folder and compile changes
+  gulp.watch(PATHS.Scss.Dir + '/**/*.scss', ['sass']);
+  //watch js folder and compile changes
+  gulp.watch(PATHS.Js.Src + '/**/*.js', ['js']);
+  //watch image folder and optimize
+  gulp.watch(PATHS.Img.Src + '/**/*', ['images']);
+});
+
+// ##################
+// Build Task
+// ##################
+
+gulp.task('build', function() {
+    runSequence(['sass']);
+});
+
+// ##################
+// Default Task
+// ##################
+
+gulp.task('default', function() {
+    runSequence(['build', 'server', 'watch']);
+});
